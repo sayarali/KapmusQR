0109f5aa471dc9d082f074e5a1ea44b2b3b76c56
Ali Sayar <alisayar07@gmail.com>
2022-12-19 22:55:28 +0300

    ÖĞretmen giriş

--- app/src/main/java/com/alisayar/kapmusqr/login/TeacherSignIn.kt
+++ app/src/main/java/com/alisayar/kapmusqr/login/TeacherSignIn.kt
@@ -5,21 +5,33 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.databinding.DataBindingUtil
+import androidx.navigation.fragment.findNavController
 import com.alisayar.kapmusqr.R
+import com.alisayar.kapmusqr.databinding.FragmentTeacherSignInBinding
 
 
 class TeacherSignIn : Fragment() {
 
 
 
-
+    private lateinit var binding: FragmentTeacherSignInBinding
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_teacher_sign_in, container, false)
+    ): View {
+        binding = DataBindingUtil.inflate(inflater, R.layout.fragment_teacher_sign_in, container, false)
+        return binding.root
     }
 
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        binding.tsiSignupButton.setOnClickListener {
+            val action = TeacherSignInDirections.actionTeacherSignInToTeacherSignUp()
+            findNavController().navigate(action)
+        }
+
+    }
 
 }
