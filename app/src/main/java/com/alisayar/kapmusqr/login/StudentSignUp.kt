4fd0ba97a9dc72d9249aeb8531cfced789677cf9
Ali Sayar <alisayar07@gmail.com>
2022-12-20 23:40:05 +0300

    Öğrenci kayıt

--- app/src/main/java/com/alisayar/kapmusqr/login/StudentSignUp.kt
+++ app/src/main/java/com/alisayar/kapmusqr/login/StudentSignUp.kt
@@ -1,24 +1,77 @@
 package com.alisayar.kapmusqr.login
 
+import android.content.Intent
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.databinding.DataBindingUtil
+import androidx.lifecycle.Observer
+import androidx.lifecycle.ViewModelProvider
+import com.alisayar.kapmusqr.MainActivity
 import com.alisayar.kapmusqr.R
+import com.alisayar.kapmusqr.databinding.FragmentStudentSignInBinding
+import com.alisayar.kapmusqr.databinding.FragmentStudentSignUpBinding
+import com.google.android.material.snackbar.Snackbar
 
 
 class StudentSignUp : Fragment() {
 
 
-
-
+    private lateinit var binding: FragmentStudentSignUpBinding
+    private lateinit var viewModel: StudentSignUpViewModel
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_student_sign_up, container, false)
+    ): View {
+        binding = DataBindingUtil.inflate(inflater, R.layout.fragment_student_sign_up, container, false)
+        viewModel = ViewModelProvider(this)[StudentSignUpViewModel::class.java]
+        binding.viewModel = viewModel
+        binding.lifecycleOwner = viewLifecycleOwner
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        binding.ssuButton.setOnClickListener {
+            viewModel.createStudent()
+        }
+
+        viewModel.studentNoError.observe(viewLifecycleOwner, Observer {
+            binding.ssuStudentNoTil.error = it
+        })
+        viewModel.emailError.observe(viewLifecycleOwner, Observer {
+            binding.ssuEmailTil.error = it
+        })
+        viewModel.nameError.observe(viewLifecycleOwner, Observer {
+            binding.ssuNameTil.error = it
+        })
+        viewModel.surnameError.observe(viewLifecycleOwner, Observer {
+            binding.ssuSurnameTil.error = it
+        })
+        viewModel.passwordError.observe(viewLifecycleOwner, Observer {
+            binding.ssuPasswordTil.error = it
+        })
+        viewModel.passwordCheckError.observe(viewLifecycleOwner, Observer {
+            binding.ssuPasswordCheckTil.error = it
+        })
+
+        viewModel.toastError.observe(viewLifecycleOwner, Observer {
+            Snackbar.make(requireView(), it, Snackbar.LENGTH_LONG).show()
+        })
+
+        viewModel.successRegisterEvent.observe(viewLifecycleOwner, Observer {
+            if(it){
+                val intent = Intent(requireContext(), MainActivity::class.java)
+                startActivity(intent)
+                viewModel.registerEventComplete()
+                requireActivity().finish()
+            }
+        })
+
+
     }
 
 }
---
+++ app/src/main/java/com/alisayar/kapmusqr/login/StudentSignUpViewModel.kt
@@ -0,0 +1,123 @@
+package com.alisayar.kapmusqr.login
+
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.FirebaseUser
+import com.google.firebase.firestore.FirebaseFirestore
+
+class StudentSignUpViewModel: ViewModel() {
+
+    private var auth: FirebaseAuth = FirebaseAuth.getInstance()
+    private var firestore: FirebaseFirestore = FirebaseFirestore.getInstance()
+
+    val studentNo = MutableLiveData<String>()
+    val email = MutableLiveData<String>()
+    val name = MutableLiveData<String>()
+    val surname = MutableLiveData<String>()
+    val phoneNumber = MutableLiveData<String>()
+    val password = MutableLiveData<String>()
+    val passwordCheck = MutableLiveData<String>()
+
+    private val _studentNoError = MutableLiveData<String>()
+    val studentNoError: LiveData<String> get() = _studentNoError
+    private val _emailError = MutableLiveData<String>()
+    val emailError: LiveData<String> get() = _emailError
+    private val _nameError = MutableLiveData<String>()
+    val nameError: LiveData<String> get() = _nameError
+    private val _surnameError = MutableLiveData<String>()
+    val surnameError: LiveData<String> get() = _surnameError
+    private val _passwordError = MutableLiveData<String>()
+    val passwordError: LiveData<String> get() = _passwordError
+    private val _passwordCheckError = MutableLiveData<String>()
+    val passwordCheckError: LiveData<String> get() = _passwordCheckError
+
+    private val _toastError = MutableLiveData<String>()
+    val toastError: LiveData<String> get() = _toastError
+
+    private val _successRegisterEvent = MutableLiveData<Boolean>()
+    val successRegisterEvent: LiveData<Boolean> get() = _successRegisterEvent
+
+    fun createStudent(){
+        var isError = false
+        if(studentNo.value.toString() == "" || studentNo.value == null){
+            _studentNoError.value = "Öğrenci numarası boş bırakılamaz."
+            isError = true
+        } else
+            _studentNoError.value = ""
+        if(email.value.toString() == "" || email.value == null){
+            _emailError.value = "E-posta boş bırakılamaz."
+            isError = true
+        } else
+            _emailError.value = ""
+        if(name.value.toString() == "" || name.value == null){
+            _nameError.value = "İsim boş bırakılamaz."
+            isError = true
+        } else
+            _nameError.value = ""
+        if(surname.value.toString() == "" || surname.value == null){
+            _surnameError.value = "Soyisim boş bırakılamaz."
+            isError = true
+        } else
+            _surnameError.value = ""
+        if(password.value.toString() == "" || password.value == null){
+            _passwordError.value = "Parola boş bırakılamaz."
+            isError = true
+        } else
+            _passwordError.value = ""
+        if(passwordCheck.value.toString() == "" || passwordCheck.value == null){
+            _passwordCheckError.value = "Parolayı tekrar giriniz."
+            isError = true
+        }
+        else if(password.value.toString() != passwordCheck.value.toString()){
+            _passwordCheckError.value = "Parolalar eşleşmiyor."
+            isError = true
+        } else
+            _passwordCheckError.value = ""
+
+        if(phoneNumber.value.toString() == "" || phoneNumber.value == null){
+            phoneNumber.value = ""
+        }
+
+        if(!isError){
+            try {
+                auth.createUserWithEmailAndPassword(email.value.toString(), password.value.toString()).addOnCompleteListener {
+                    if(it.isSuccessful){
+                        createUserCollection(auth.currentUser!!)
+                    }
+                }.addOnFailureListener {
+                    _toastError.value = it.localizedMessage
+                }
+            } catch (e: Exception){
+                println(e)
+            }
+        }
+    }
+    private fun createUserCollection(user: FirebaseUser){
+
+        try {
+            val teacherHashMap = HashMap<String, Any>()
+            teacherHashMap["id"] = user.uid
+            teacherHashMap["personalNo"] = studentNo.value.toString()
+            teacherHashMap["email"] = email.value.toString()
+            teacherHashMap["name"] = name.value.toString()
+            teacherHashMap["surname"] = surname.value.toString()
+            teacherHashMap["phoneNumber"] = phoneNumber.value.toString()
+
+            firestore.collection("Students").document(user.uid).set(teacherHashMap).addOnCompleteListener {
+                _successRegisterEvent.value = true
+            }.addOnFailureListener {
+                _toastError.value = it.localizedMessage
+            }
+
+
+        } catch (e: Exception){
+            println(e)
+        }
+    }
+    fun registerEventComplete(){
+        _successRegisterEvent.value = false
+    }
+
+}
