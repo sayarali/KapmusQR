c921bbf89070115c14c01ee95d3e0cc918f03b93
Ali Sayar <alisayar07@gmail.com>
2022-12-25 21:00:02 +0300

    Öğretmen

---
+++ app/src/main/java/com/alisayar/kapmusqr/teacher/TeacherMainActivity.kt
@@ -0,0 +1,12 @@
+package com.alisayar.kapmusqr.teacher
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.alisayar.kapmusqr.R
+
+class TeacherMainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_teacher_main)
+    }
+}
