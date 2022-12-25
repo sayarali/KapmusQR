9d577f59dedc0a32e57abf0ef899c6dbebad9126
Ali Sayar <alisayar07@gmail.com>
2022-12-25 20:59:38 +0300

    Öğrenci

---
+++ app/src/main/java/com/alisayar/kapmusqr/student/StudentMainActivity.kt
@@ -0,0 +1,14 @@
+package com.alisayar.kapmusqr.student
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.alisayar.kapmusqr.R
+
+class StudentMainActivity : AppCompatActivity() {
+
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_student_main)
+    }
+}
---
+++ app/src/main/java/com/alisayar/kapmusqr/student/myLessons/StudentMyLessonsFragment.kt
@@ -0,0 +1,23 @@
+package com.alisayar.kapmusqr.student.myLessons
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.alisayar.kapmusqr.R
+
+
+class StudentMyLessonsFragment : Fragment() {
+
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_student_my_lessons, container, false)
+    }
+
+
+}
